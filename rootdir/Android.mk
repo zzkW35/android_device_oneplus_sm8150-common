d10e8926d10d8f9377a628a4165d65e5628eaf1b
Filippo <zzkw35@gmail.com>
2020-09-21 18:20:42 +0200

    sm8150-common: Rename ueventd

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -222,11 +222,11 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
