b5ed650f51a32d4f4257393f451f6b27b31fee36
Filippo <zzkw35@gmail.com>
2020-11-05 21:53:36 +0100

    sm8150-common: Build android.hardware.radio.deprecated
    Change-Id: I765479c8305757a4d8a931aa3a3eaefefa9fc539

--- common.mk
+++ common.mk
@@ -404,6 +404,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio.config@1.2 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
     librmnetctl \
